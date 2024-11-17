12ac465b2569d0eddc6ac278a1d981a8e981cb58
tranducluong <tranducluong886@gmail.com>
2024-11-17 21:36:51 +0700

    fix conflict feature/21 + 27

--- src/app/service/ApiConfigService.ts
+++ src/app/service/ApiConfigService.ts
@@ -5,11 +5,11 @@ import { Injectable } from '@angular/core';
 })
 export class ApiConfigService {
 
-  public static apiUrlSocket ="http://192.168.0.193:8080" ;
-  public static apiUrl ="http://192.168.0.193:8080" ;
-  public static apiUrlPromotion ="http://192.168.0.193:8080/api/v1/promotions" ;
-  public static apiFoods ="http://192.168.0.193:8080/api/v1/foods";
- public static apiUrlimg ="http://192.168.0.193:8080/images/";
+  public static apiUrl ="http://192.168.2.5:8080" ;
+  public static apiUrlPromotion ="http://192.168.2.5:8080/api/v1/promotions" ;
+  public static apiFoods ="http://192.168.2.5:8080/api/v1/foods";
+ public static apiUrlimg ="http://192.168.2.5:8080/images/";
+  public static apiUrlSocket ="http://192.168.2.5:8080" ;
 
 }
 
